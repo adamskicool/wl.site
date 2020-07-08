04aefc808425467b94acdc77bccfe4e554612854
Adam Torkkeli <adam.torkkeli@getaccept.com>
2020-07-08 19:37:27 +0200

    fix(linting)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <vue-page-transition
-      name="fade-in-up"
-      class="router-wrapper"
-    >
+    <vue-page-transition name="fade-in-up" class="router-wrapper">
       <router-view />
     </vue-page-transition>
     <Alert />
@@ -11,25 +8,20 @@
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue,
-  Prop,
-} from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 import Alert from "@/components/alert/Alert.vue";
 
 @Component({
   components: {
-    Alert,
-  },
+    Alert
+  }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica,
-    Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/store/modules/session/session.mutations.ts
+++ src/store/modules/session/session.mutations.ts
@@ -1,14 +1,10 @@
 import { MutationTree } from "vuex";
 import { ISessionState } from "./session.store";
 
-export const mutationSetToken: string =
-  "setToken";
+export const mutationSetToken: string = "setToken";
 
 export const mutations: MutationTree<ISessionState> = {
-  [mutationSetToken](
-    state: ISessionState,
-    token: string
-  ) {
+  [mutationSetToken](state: ISessionState, token: string) {
     state.token = token;
-  },
+  }
 };
